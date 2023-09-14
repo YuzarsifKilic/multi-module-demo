ea7b5c9168a08de0b92943c22a9dc2132a4ff271
Yusuf Kılıç <yuzarsifkilic@gmail.com>
2023-09-14 21:56:40 +0300

    init commit

---
+++ src/main/java/com/yuzarsif/multimoduledemo/MultiModuleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yuzarsif.multimoduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuzarsif/multimoduledemo/MultiModuleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuzarsif.multimoduledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
